f6a5882e38acea3248c6fc0091cb4b3a3de0aaec
lol768 <support@technostripe.com>
2013-04-26 18:26:33 +0100

    Add a version check.

--- src/com/modcrafting/mbd/Chekkit.java
+++ src/com/modcrafting/mbd/Chekkit.java
@@ -70,6 +70,7 @@ public class Chekkit extends JFrame implements WindowListener {
     private Map<String, String> keyword = new HashMap<String, String>();
     private List<String> bannedpackage = new ArrayList<String>();
     private static Theme theme;
+    public static final String VERSION = "1.5";
     public static String PATH = new File(Chekkit.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath().replace(File.separator + "Chekkit.jar", "").replace(File.separator + "mbd.jar",  "");
 
     @SuppressWarnings("unchecked")
@@ -458,7 +459,7 @@ public class Chekkit extends JFrame implements WindowListener {
             }
             this.dispose();
             ImageIcon img = new ImageIcon(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/resources/bukkit-icon-small.png")));
-            JOptionPane.showMessageDialog(null, "Chekkit or Die.\nVersion: 0.4\nDeathmarine, lol768, zeeveener", "Good Bye.", JOptionPane.PLAIN_MESSAGE, img);
+            JOptionPane.showMessageDialog(null, "Chekkit or Die.\nVersion: " + Chekkit.VERSION + "\nDeathmarine, lol768, zeeveener", "Good Bye.", JOptionPane.PLAIN_MESSAGE, img);
             System.exit(0);
         }
     }
