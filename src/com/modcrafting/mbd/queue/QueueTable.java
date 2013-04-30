96121f18fb1ddbd0ae646143c8e79a21fe9e9d48
lol768 <support@technostripe.com>
2013-04-30 17:13:35 +0100

    Fix build issue with PrettyTime

--- src/com/modcrafting/mbd/queue/QueueTable.java
+++ src/com/modcrafting/mbd/queue/QueueTable.java
@@ -2,7 +2,7 @@
 
 import javax.swing.table.AbstractTableModel;
 
-class QueueTable extends AbstractTableModel {
+public class QueueTable extends AbstractTableModel {
     
     private String[] columnNames = { "First Name", "Last Name", "Sport", "# of Years", "Vegetarian" };
     private Object[][] data = { { "Kathy", "Smith", "Snowboarding", new Integer(5), new Boolean(false) }, { "John", "Doe", "Rowing", new Integer(3), new Boolean(true) }, { "Sue", "Black", "Knitting", new Integer(2), new Boolean(false) }, { "Jane", "White", "Speed reading", new Integer(20), new Boolean(true) }, { "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) } };
