c512513ee349e91558443b0d70d8ee4e0543ab25
lol768 <support@technostripe.com>
2013-05-12 13:06:45 +0100

    Hopefully add Jenkins build number to Chekkit.java

--- src/com/modcrafting/mbd/Chekkit.java
+++ src/com/modcrafting/mbd/Chekkit.java
@@ -75,6 +75,7 @@ public class Chekkit extends JFrame implements WindowListener {
     private List<String> bannedpackage = new ArrayList<String>();
     private static Theme theme;
     public static final String VERSION = "1.5";
+    public static final String JENKINS_BUILD = "#JENKINSBUILDNUMBER";
     public static Boolean showAbout = true;
     public static String bukkitDevUsername;
     public static String chekkitUsername;
